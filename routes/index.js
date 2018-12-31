5a89a442b0893b801a8e76e36d9ad3ba20aa7281
Ramakrishna Reddy <ramakrishnareddy.bareddy@outlook.com>
2018-12-31 14:33:17 +0530

    first commit

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+router.post("/echo", function (req, res) {
+  var speech =
+    req.body.result &&
+      req.body.result.parameters &&
+      req.body.result.parameters.echoText
+      ? req.body.result.parameters.echoText
+      : "Seems like some problem. Speak again.";
+  return res.json({
+    speech: speech,
+    displayText: speech,
+    source: "webhook-echo-sample"
+  });
+});
+module.exports = router;
