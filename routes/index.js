d3fe8df782baa07604bef5ac6d0bf62c7c9aad36
Ramakrishna Reddy <ramakrishnareddy.bareddy@outlook.com>
2019-01-08 11:19:37 +0530

    first commit

--- routes/index.js
+++ routes/index.js
@@ -14,10 +14,43 @@ router.post("/echo", function (req, res) {
       : "Seems like some problem. Speak again.";
   console.log(req.body);
   return res.json({
-   
-    displayText: speech,
     source: "webhook-echo-sample"
   });
+});
+router.get('/getName', function (req, res) {
+  res.send('Swarup Bam');
+});
+router.post('/getMovies', function (request, response) {
+  if (request.body.result.parameters['top-rated']) {
+    var req = unirest("GET", "https://api.themoviedb.org/3/movie/top_rated");
+    req.query({
+      "page": "1",
+      "language": "en-US",
+      "api_key": ""
+    });
+    req.send("{}");
+    req.end(function (res) {
+      if (res.error) {
+        response.setHeader('Content-Type', 'application/json');
+        response.send(JSON.stringify({
+          "speech": "Error. Can you try it again ? ",
+          "displayText": "Error. Can you try it again ? "
+        }));
+      } else if (res.body.results.length > 0) {
+        let result = res.body.results;
+        let output = '';
+        for (let i = 0; i < result.length; i++) {
+          output += result[i].title;
+          output += "\n"
+        }
+        response.setHeader('Content-Type', 'application/json');
+        response.send(JSON.stringify({
+          "speech": output,
+          "displayText": output
+        }));
+      }
+    });
+  }
 });
 
 module.exports = router;
